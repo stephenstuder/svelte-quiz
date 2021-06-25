8f033f5e44c9628d0d6979bd4847c26933ca6a4f
Stephen Studer <stephen_studer@trekbikes.com>
2021-06-24 19:02:34 -0700

    learning components and props, holy smokes this is nice

---
+++ src/Quiz.svelte
@@ -0,0 +1,13 @@
+<script>
+  import QuizQuestion from "./QuizQuestion.svelte";
+
+  export let quizName = "Stephen Quiz";
+  let title = "hello";
+</script>
+
+<div>
+  <h2>{quizName}</h2>
+  <h4>{title}</h4>
+  <input bind:value={title} type="text" />
+  <QuizQuestion answer="hello" />
+</div>
---
+++ src/QuizQuestion.svelte
@@ -0,0 +1,5 @@
+<script>
+  export let answer = "default";
+</script>
+
+<p>{answer}</p>
